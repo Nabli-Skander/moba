2fce8f80ad84489a3e99a4fda636ad8e758e2b14
Tim00000000000000 <50807279+timdu83@users.noreply.github.com>
2020-06-05 10:49:23 +0200

    First commit

---
+++ Source/Steam/SteamGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SteamGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASteamGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASteamGameMode();
+};
+
+
+
