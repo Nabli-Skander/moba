baca64039d0c3a5de8dfad94657e6cb020e11e62
Shibachou <xitid.gamer@gmail.com>
2020-06-19 17:33:20 +0200

    change ThirdPersonCPP vers Personnage

--- Source/Steam/SteamGameMode.cpp
+++ Source/Steam/SteamGameMode.cpp
@@ -7,7 +7,7 @@
 ASteamGameMode::ASteamGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Personnage/Blueprints/ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
